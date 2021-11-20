52545076ccc880e85570e9ee72af322d67e5944c
Alexey Rodionov <fluorescent.hallucinogen@gmail.com>
2021-11-20 11:16:59 +0300

    Add "Install PWA" and "Show notification" buttons

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener('fetch', () => {
+  // Empty fetch event handler to bypass PWA installability criteria (https://web.dev/install-criteria/#criteria).
+});
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+
+  if (!event.action) {
+    event.waitUntil(clients.openWindow(event.notification.data.url));
+  }
+});
